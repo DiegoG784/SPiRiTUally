5e4c4f06d758758d5bcac5722e142613d132494f
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-09-01 12:28:16 -0300

    Mudanças nos managers
    adicionado um pouco de código em alguns dos managers

--- entities/base/components/health_manager.gd
+++ entities/base/components/health_manager.gd
@@ -2,5 +2,12 @@ extends Node
 
 var health
 var defense
-
 var max_health
+
+signal health_depleted
+
+func heal(value):
+	pass
+
+func damage(value):
+	pass
---
+++ managers/sound_manager/sound_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+onready var music = get_node("music_player")
+onready var sfx = get_node("sfx")
