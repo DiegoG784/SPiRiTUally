d1f36e901d14f70ea994b15a4865010c3cfedab2
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-09-19 18:20:46 -0300

    Alteração nos arquivos

--- entities/base/components/scene/scene_changer.gd
+++ entities/base/components/scene/scene_changer.gd
@@ -1,5 +1,5 @@
-extends Node
-
+extends Area
+class_name SceneSwitcher
 
 # Declare member variables here. Examples:
 # var a = 2
--- managers/scene_manager/scene_manager.gd
+++ managers/scene_manager/scene_manager.gd
@@ -7,3 +7,5 @@ export(bool) var mute_music_when_entering
 func _ready():
 	Game.set_current_scene(self)
 
+func change_scene(scene_file):
+	pass
