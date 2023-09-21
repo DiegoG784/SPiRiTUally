6238e800f4a6f39eb550e2a7d88ac0673957d9b3
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-09-21 14:54:56 -0300

    Adição de código no item_manager
    Agora o gerenciador possui um método de procura de itens e adição do mesmo

--- managers/item_manager.gd
+++ managers/item_manager.gd
@@ -6,11 +6,43 @@ var inventory = [
 ]#Ver como vai ser guardado os itens do jogo (através de recursos ou algo mais complexo envolvendo arrays?)
 
 
-func add_item(item_name):
-	pass
+func add_item(item_file, quantity = 0):
+	var item_position = search_item(item_file.name, true)
+	
+	if quantity > 0:
+		
+		if item_position != null:
+			inventory[item_position][1] += quantity
+		else:
+			inventory.append_array([item_file, quantity])
+		
+	else:
+		
+		if item_position != null:
+			inventory.append(item_file)
+		else:
+			printerr("O item é único já possui no inventário!")
 
 func remove_item(item_name):
 	pass
 
 func use_item(item_name, quantity):
 	pass
+
+func search_item(item_name, return_array_position = false):
+	pass
+	for item in inventory:
+		if item is Array:
+			
+			if item[0].name == item_name:
+				if return_array_position:
+					return item
+				else:
+					return item_name
+			
+		elif item.name == item_name:
+			if return_array_position:
+				return item
+			else:
+				return item_name
+	return null
