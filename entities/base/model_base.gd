a841d49d4d041ff464e8b895947f9f43b94da481
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-10-20 14:05:02 -0300

    Aplicação de Shaders
    - Uma classe foi criada para aplicar os shaders necessários automaticamente nos modelos, sem a necessidade de aplicar manualmente, mais aprimoramentos terão que ser feitos

---
+++ entities/base/model_base.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+var shader = load("res://entities/shaders/psx_shaders/psx_lit.shader")
+
+func _ready():
+#	print(shader)
+	
+	for mesh in get_children():
+		if mesh is MeshInstance:
+			print(mesh)
+			pass
+			mesh.material_override = ShaderMaterial.new()
+			mesh.material_override.shader = shader
+#			yield(get_tree(),"idle_frame")
+			mesh.material_override.set_shader_param("shader_param/albedoTex", mesh.mesh.get("surface_1/material").albedo_texture)
