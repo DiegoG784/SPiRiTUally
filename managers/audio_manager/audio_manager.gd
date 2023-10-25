317dcd106c2878c426787b88d641942a504de078
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-10-25 18:25:57 -0300

    Renomeação de múltiplas pastas e arquivos e outros
    - Um sistema para tocar os efeitos sonoros simples foi criado para o projeto. Será necessário revisões e testes para estar considerado pronto

--- managers/audio_manager/audio_manager.gd
+++ managers/audio_manager/audio_manager.gd
@@ -6,9 +6,41 @@ extends Node
 onready var music = get_node("music_player")
 onready var sfx = get_node("sfx")
 
+func _ready():
+	pass
+	var dir = Directory.new()
+	dir.open("res://assets/audio/")
+	dir.list_dir_begin(true)
+	var file_name = dir.get_next()
+
+	while file_name != "":
+		
+		if dir.current_is_dir():
+			print(file_name)
+			list_files_in_dir(file_name)
+		else:
+			pass
+			print(file_name)
+		
+		file_name = dir.get_next()
+
+
 func play_music(music_name):
 	pass
 
-func play_audio(audio_name):
+func play_audio(audio_file):
 	var audio = AudioStreamPlayer.new()
-	
+	audio.stream = audio_file
+	audio.connect("finished", self, "audio_finished", [audio])
+	audio.pause_mode = PAUSE_MODE_PROCESS
+	sfx.add_child(audio)
+	audio.play()
+
+func load_file(audio_path):
+	pass
+
+func audio_finished(audio):
+	audio.queue_free()
+
+func list_files_in_dir(path):
+	pass
