7cde8835b0cc89e5b7987ae1c97b976e3c892c51
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-10-27 18:23:21 -0300

    Desenvolvimento inicial da introdução do jogo

--- managers/scene_manager/scene_manager.gd
+++ managers/scene_manager/scene_manager.gd
@@ -4,6 +4,8 @@ onready var gui = get_node("gui")
 onready var current_scene_node = get_node("current_scene")
 
 var current_camera
+var transition_overlay_node = preload("res://gui/scene/scene_transition_black.tscn")
+
 
 func _ready():
 	Game.set_scene_manager(self)
@@ -15,25 +17,33 @@ func add_scene_to_main(scene_file):
 func change_scene(scene_file):
 	current_camera = null
 	var next_scene = scene_file.instance()
-	var transition = preload("res://gui/scene/scene_transition_black.tscn").instance()
+	var transition_overlay = transition_overlay_node.instance()
 
-	gui.add_child(transition)
-	transition.fade_in()
+	gui.add_child(transition_overlay)
+	transition_overlay.fade_in()
 
-	yield(transition.animation, "animation_finished")
+	yield(transition_overlay.animation, "animation_finished")
 	
 	Game.current_scene.queue_free()
 	current_scene_node.add_child(next_scene)
 
 	yield(get_tree(), "idle_frame")
 	yield(get_tree().create_timer(0.3), "timeout")
-	transition.fade_out()
+	transition_overlay.fade_out()
 	set_current_camera(get_viewport().get_camera())
 #	print(current_camera)
-	yield(transition.animation, "animation_finished")
+	yield(transition_overlay.animation, "animation_finished")
+	transition_overlay.queue_free()
 
 func set_current_camera(camera):
 	current_camera = camera
 
 func get_current_camera():
 	return current_camera
+
+func fade_in_screen():
+	var transition = transition_overlay_node.instance()
+	
+
+func fade_out_screen():
+	pass
