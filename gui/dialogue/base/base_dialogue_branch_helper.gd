8a9df08b7de93d936d5d318615005ccf85a99704
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-10-31 14:54:03 -0300

    Desenvolvimento da Introdução do jogo

--- gui/dialogue/base/base_dialogue_branch_helper.gd
+++ gui/dialogue/base/base_dialogue_branch_helper.gd
@@ -3,4 +3,4 @@ extends CanvasLayer
 onready var dialogue_window = get_node("base_dialogue_branch")
 
 func setup_signals(target):
-    dialogue_window.connect("dialogue_finished", target, "dialogue_finished")
+	dialogue_window.connect("dialogue_finished", target, "dialogue_finished")
