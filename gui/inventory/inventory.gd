50e46abf8fbbf6a6962b7a66d737c9b31ee86678
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2023-12-05 18:20:01 -0300

    Desenvolvimento do menu do jogo

--- gui/inventory/inventory.gd
+++ gui/inventory/inventory.gd
@@ -1,18 +1,44 @@
 extends Control
 
-onready var item_list = get_node("item_list")
+onready var item_lister = get_node("background/middle_separator/item_lister_frame/item_lister")
 
 var selected_item
+var current_menu_list
+
 
 func _ready():
+	Inventory.give_item(load("res://database/items/weapons/axe.tres"))
+	refresh_inventory("equipment")
+#	for item in Inventory.inventory:
+#		
+#		if item is Array:
+#			item_list.add_item("%sx %s" % [item[1], item[0].name])
+#		else:
+#			item_list.add_item(item.name)
+
+func refresh_inventory(type):
 	for item in Inventory.inventory:
-		
-		if item is Array:
-			item_list.add_item("%sx %s" % [item[1], item[0].name])
-		else:
-			item_list.add_item(item.name)
-
-
+		var item_list = preload("res://gui/inventory/list_item/list_item.tscn").instance()
+		match type:
+			"equipment":
+				if !item is Array:
+#					print("oioi")
+					if item.get_class() == "WeaponItem":
+						item_lister.add_child(item_list)
+						item_list.setup(item.icon, item.name)
+			"item":
+				if item is Array:
+#					print("oioi")
+					if item.get_class() == "Item" or item.get_class() == "ConsumableItem":
+						item_lister.add_child(item_list)
+						item_list.setup(item.icon, item.name)
+						
+			"evidence":
+				if !item is Array:
+#					print("oioi")
+					if item.get_class() == "EvidenceItem":
+						item_lister.add_child(item_list)
+						item_list.setup(item.icon, item.name)
 
 func item_selected(index): #Usado quando o item é clicado na lista
 	selected_item = Inventory.inventory[index]
